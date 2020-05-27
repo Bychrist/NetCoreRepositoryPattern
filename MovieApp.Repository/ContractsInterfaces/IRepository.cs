18f38cfaa3c44a606f7deb6b9d8d5ec9d6a957c2
Bychrist <ordained07@yahool.com>
2020-05-27 12:46:34 +0100

    first commit

---
+++ MovieApp.Repository/ContractsInterfaces/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MovieApp.Repository.ContractsInterfaces
+{
+   public  interface IRepository <T> where T:class
+    {
+        IEnumerable<T> GetAll();
+
+        T GetById(int id);
+
+        void Update(T entity);
+
+        void Delete(T entity);
+
+
+    }
+}
